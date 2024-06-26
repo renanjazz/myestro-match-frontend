053e92955399409a8836135d9fa3c1624403e42a
DaniDias95 <guiguimu_art@hotmail.com>
2024-06-26 12:00:37 +0100

    footer styling and creatingcreation and basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
 // React tools
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react';
+import './styles/App.css';
+import { Routes, Route } from 'react-router-dom';
 // Components
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
-import ScheduleCard from "./components/ScheduleCard";
-import SchedulePage from "./pages/SchedulePage";
-import StudioCard from "./components/StudioCard";
-import TeacherCard from "./components/TeacherCard";
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import ScheduleCard from './components/ScheduleCard';
+import StudioCard from './components/StudioCard';
+import TeacherCard from './components/TeacherCard';
 // Pages
-import AboutPage from "./pages/AboutPage";
-import LandingPage from "./pages/LandingPage";
-import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/SignupPage";
-import SchedulePage from "./pages/SchedulePage";
-import StudiosPage from "./pages/StudiosPage";
-import TeacherPage from "./pages/TeacherPage";
-import UserProfilePage from "./pages/UserProfilePage";
+import AboutPage from './pages/AboutPage';
+import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import SchedulePage from './pages/SchedulePage';
+import StudiosPage from './pages/StudiosPage';
+import TeacherPage from './pages/TeacherPage';
+import UserProfilePage from './pages/UserProfilePage';
 
 function App() {
-  return (
-    <>
-      <div>
-        <Navbar/>
-        <Routes>
-
-        </Routes>
-        <Footer/>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div>
+				<Navbar />
+				<Routes></Routes>
+				<Footer />
+			</div>
+		</>
+	);
 }
 
 export default App;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className='footer'>
+      <div className='footer-about-contact'>
+        <br />
+        <NavLink to=""><h2>About us</h2></NavLink>
+        <NavLink to=""><h2>Contact us</h2></NavLink>
+      </div>
+      <div className='footer-drj-product'>
+        <br />
+        <br />
+        <p><em>A product of <br />
+        DRJ Industries</em></p>
+      </div>
     </div>
   )
 }
