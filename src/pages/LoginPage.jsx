4d10c540fbe2fbf6ca0816b389f11c2c3ff56af5
DaniDias95 <guiguimu_art@hotmail.com>
2024-06-27 15:02:53 +0100

    login fixed

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, { useContext, useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { API_URL } from '../config.js';
+import { AuthContext } from '../context/auth.context.jsx';
 
 const LoginPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const nav = useNavigate();
+  const { storeToken, authUser } = useContext(AuthContext);
+
+  async function handleLogin(e) {
+    e.preventDefault();
+    const userInfo = { username, password };
+    try {
+      const { data } = await axios.post(`${API_URL}/auth/login`, userInfo);
+      await storeToken(data.token);
+      await authUser();  
+      nav('/');
+    } catch (error) {
+      console.error('Error logging in:', error);
+      setError('Failed to login. Please check your credentials.');
+    }
+  }
+
   return (
     <div>
-      
+      <h2>Welcome back!</h2>
+      <form onSubmit={handleLogin}>
+        <label>
+          Username:
+          <input
+            name="username"
+            type="text"
+            value={username}
+            placeholder='your username'
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            name="password"
+            type="password"
+            value={password}
+            placeholder='your password'
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </label>
+        <button type="submit">Login!</button>
+      </form>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
