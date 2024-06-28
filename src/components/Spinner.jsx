09418a40328ce0a3a9db9908d08ce942a2562eaa
Renan <renan.castro@dashlane.com>
2024-06-28 09:40:33 +0100

    isprivate

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Spinner from "react-bootstrap/Spinner";
+
+const MySpinner = () => {
+  return <Spinner animation="grow" variant="dark" id="spinner"/>;
+};
+
+export default MySpinner;
