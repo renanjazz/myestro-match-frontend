113c86b041fa100cc7c191862cfab342bf666208
Jonathan Hingorani <jonathanhingorani@gmail.com>
2024-07-01 11:16:34 +0200

    weekend css

--- src/pages/TeacherPage.jsx
+++ src/pages/TeacherPage.jsx
@@ -23,9 +23,9 @@ const TeacherPage = () => {
 
   return (
     <>
-    <div>
+    <div className="teachers-page">
       <h2>Teachers</h2>
-      <h4>{teachers.length} Teachers found</h4>
+      <h4>{teachers.length} teachers found</h4>
       {teachers.map((teacher)=><TeacherCard key={teacher._id} teacher={teacher} />)}
     </div>
     </>
