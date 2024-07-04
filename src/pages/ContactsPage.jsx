7c00d41056decaf403b345ae9b94462fb2203a87
Renan <renan.castro@dashlane.com>
2024-07-04 13:42:44 +0100

    contacts new styling

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import dani from '../assets/dani.png'
+import jonny from '../assets/jonny.png'
+import renan from '../assets/renan.png'
 
 const ContactsPage = () => {
 	return (
@@ -13,9 +16,14 @@ const ContactsPage = () => {
 				<p className='li-github-link'>Myestro Back-end</p>
 			</a>
 			<div>
-     	 		<section>
+     	 		<section className='section-contacts'>
 					{' '}
 					<h3>Daniela Dias</h3>
+					<div id="contact-picture">
+					<img className="teacher-studio-picture" src={dani} alt="daniela" />
+					</div>
+			
+				
 					<a href="https://www.linkedin.com/in/daniela-dias-604926243/">
 						<p className='li-github-link'>Linkedin</p>
 					</a>
@@ -23,18 +31,25 @@ const ContactsPage = () => {
 						<p className='li-github-link'>Github</p>
 					</a>
 				</section>
-				<section>
+				
+				<section className='section-contacts'>
 					<h3>Renan Castro</h3>
+					<div id="contact-picture">
+					<img className="teacher-studio-picture" src={renan} alt="renan" />
+					</div>
 					<a href="">
 						<p className='li-github-link'>Linkedin</p>
 					</a>
 					<a href="https://github.com/renanjazz">
 						<p className='li-github-link'>Github</p>
 					</a>
-				</section>
-				<section>
+				</section >
+				<section className='section-contacts'>
 					{' '}
 					<h3>Jonathan Hingorani</h3>
+					<div id="contact-picture">
+					<img className="teacher-studio-picture" src={jonny} alt="jonny" />
+					</div>
 					<a href="">
 						<p className='li-github-link'>Linkedin</p>
 					</a>
